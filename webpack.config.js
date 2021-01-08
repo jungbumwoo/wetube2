df6130af509a2c0200df2911622bf15c0cf26605
jungbumwoo <jungbumwoo@naver.com>
2021-01-08 14:08:27 +0900

    webpack err -ing

--- webpack.config.js
+++ webpack.config.js
@@ -1,15 +1,14 @@
 const path = require('path');
+const autoprefixer = require("autoprefixer");
 const MiniCssExtractPlugin = require('mini-css-extract-plugin');
 const MODE = process.env.WEBPACK_ENV;
 
-module.exports = {
-    entry: path.resolve(__dirname, "assets", "js", "main.js"),
+const ENTRY_FILE = path.resolve(__dirname, "assets", "js", "main.js");
+const OUTPUT_DIR = path.join(__dirname, "static");
+
+const config = {
+    entry: ["@babel/polyfill", ENTRY_FILE],
     mode: MODE,
-    output: {
-        filename: '[name].js',
-        path: path.resolve(__dirname, 'static'),
-    },
-    plugins: [new MiniCssExtractPlugin()],
     module: {
         rules: [
             {
@@ -23,28 +22,48 @@ module.exports = {
                 }
             },
             {
-                test: /\.s[ac]ss$/i,
+                test: /\.(scss)$/,
                 use: [
                     // Create `style` nodes from JS strings
                     
                     // Translate CSS into CommonJS
                     {
-                        loader: MiniCssExtractPlugin.loader,
+                        loader: MiniCssExtractPlugin.loader
                     },
                     {
                         loader: "css-loader",
                     },
                     {
                         loader: "postcss-loader",
+                        options: {
+                            postcssOptions: {
+                                plugins: [
+                                    [
+                                        'autoprefixer',
+                                        {
+                                            browsers: "cover 99.5%"
+                                        }
+                                    ]
+                                ]
+                            }
+                        }
                     },
                     {   // Compiles Sass to Css
-                        loader: "sass-loader",
-                        options: {
-                            implementation: require("sass")
-                        },
+                        loader: "sass-loader"
                     },
                 ],
             }
         ]
-    }
+    },
+    output: {
+        filename: '[name].js',
+        path: OUTPUT_DIR,
+    },
+    plugins: [
+        new MiniCssExtractPlugin({
+        filename: '[name].css',
+        })
+    ],
 }; 
+
+module.exports = config;
