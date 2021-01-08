1473f3b710cbffd6426b41aad46b20c210b045ea
jungbumwoo <jungbumwoo@naver.com>
2021-01-08 18:52:18 +0900

    webpack...

--- webpack.config.js
+++ webpack.config.js
@@ -1,69 +1,67 @@
-const path = require('path');
+const path = require("path");
 const autoprefixer = require("autoprefixer");
 const MiniCssExtractPlugin = require('mini-css-extract-plugin');
-const MODE = process.env.WEBPACK_ENV;
 
+const MODE = process.env.WEBPACK_ENV;
 const ENTRY_FILE = path.resolve(__dirname, "assets", "js", "main.js");
 const OUTPUT_DIR = path.join(__dirname, "static");
 
 const config = {
-    entry: ["@babel/polyfill", ENTRY_FILE],
-    mode: MODE,
-    module: {
-        rules: [
-            {
-                test: /\.m?js$/,
-                exclude: /(node_modules|bower_components)/,
-                use: {
-                    loader: 'babel-loader',
-                    options: {
-                        presets: ['@babel/preset-env']
-                    }
-                }
-            },
-            {
-                test: /\.(scss)$/,
-                use: [
-                    // Create `style` nodes from JS strings
-                    
-                    // Translate CSS into CommonJS
-                    {
-                        loader: MiniCssExtractPlugin.loader
-                    },
-                    {
-                        loader: "css-loader",
-                    },
-                    {
-                        loader: "postcss-loader",
-                        options: {
-                            postcssOptions: {
-                                plugins: [
-                                    [
-                                        'autoprefixer',
-                                        {
-                                            browsers: "cover 99.5%"
-                                        }
-                                    ]
-                                ]
-                            }
-                        }
-                    },
-                    {   // Compiles Sass to Css
-                        loader: "sass-loader"
-                    },
-                ],
-            }
+  entry: ["@babel/polyfill", ENTRY_FILE],
+  mode: MODE,
+  module: {
+    rules: [
+      {
+        test: /\.(js)$/,
+        use: [
+          {
+            loader: "babel-loader"
+          }
         ]
-    },
-    output: {
-        filename: '[name].js',
-        path: OUTPUT_DIR,
-    },
-    plugins: [
-        new MiniCssExtractPlugin({
-        filename: '[name].css',
-        })
-    ],
-}; 
+      },
+      {
+        test: /\.(scss)$/,
+        use: [
+          {
+            loader: MiniCssExtractPlugin.loader
+          },
+          {
+            loader: "css-loader"
+          },
+          {
+            loader: "postcss-loader",
+            options: {
+              postcssOptions: {
+                plugins: [
+                  [
+                    'autoprefixer',
+                    {
+                      //options
+                      browsers: "cover 99.5%"
+                    },
+                  ]
+                ]
+              }
+            }
+          },
+          {
+            loader: "sass-loader"
+          }
+        ]
+      }
+    ]
+  },
+  output: {
+    path: OUTPUT_DIR,
+    filename: "[name].js"
+  },
+  plugins: [
+    new MiniCssExtractPlugin({
+      // Options similar to the same options in webpackOptions.output
+      // both options are optional
+      filename: '[name].css',
+    }),
+  ]
+};
 
 module.exports = config;
